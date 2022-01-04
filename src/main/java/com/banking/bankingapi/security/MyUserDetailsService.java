8a57fbe57e8d160464c8b6a12be82f48037fe091
Eric Brown <ericbrown589@yahoo.com>
2022-01-04 12:05:06 -0600

    setup security

---
+++ src/main/java/com/banking/bankingapi/security/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.banking.bankingapi.security;
+
+
+import com.banking.bankingapi.model.User;
+import com.banking.bankingapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(email);
+        return new MyUserDetails(user);
+    }
+}
