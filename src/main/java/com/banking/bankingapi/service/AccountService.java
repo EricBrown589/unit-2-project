e910f895744b4f9c2674b2fd0d11945700af3837
diaba <hwar@cincinnatistate.edu>
2022-01-06 15:40:06 -0500

    working on total balance withdraw

--- src/main/java/com/banking/bankingapi/service/AccountService.java
+++ src/main/java/com/banking/bankingapi/service/AccountService.java
@@ -5,8 +5,10 @@ import com.banking.bankingapi.exception.InformationNotFoundException;
 import com.banking.bankingapi.exception.InsufficientResources;
 import com.banking.bankingapi.model.Account;
 import com.banking.bankingapi.model.Transaction;
+import com.banking.bankingapi.model.User;
 import com.banking.bankingapi.repository.AccountRepository;
 import com.banking.bankingapi.repository.TransactionRepository;
+import com.banking.bankingapi.repository.UserRepository;
 import com.banking.bankingapi.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,6 +24,7 @@ public class AccountService {
 
   private AccountRepository accountRepository;
  private TransactionRepository transactionRepository;
+ private UserRepository userRepository;
 
 
   private static final Logger LOGGER = Logger.getLogger(AccountService.class.getName());
@@ -38,11 +41,10 @@ public class AccountService {
     this.transactionRepository = transactionRepository;
   }
 
-
-
-
-
-
+  @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
 //  *******************   ACCOUNTS COMPLETED   *******************
 
@@ -72,19 +74,30 @@ public class AccountService {
     }
   }
 
-  //  *** CREATE ACCOUNT
+
+
+    /**
+     * <h1>CREATE ACCOUNT</h1>
+     * <p>Check if the account is not already in the database</p>
+     * <p>Update the totalBalance of the user </p>
+     * @param accountObject
+     * @return
+     */
   public Account createAccount(Account accountObject) {
     MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     LOGGER.info("`AccountService` Creating Account....");
+    User user = userDetails.getUser();
     Account account = accountRepository.findByIdAndUserId(userDetails.getUser().getId(), accountObject.getId());
     if(account != null) {
       throw new InformationExistsException("Account Id Already In Use: " + account.getId());
     } else {
       accountObject.setUser(userDetails.getUser()); // set account.user to userID
+        //update user balance
+        user.setTotalBalance(user.getTotalBalance() + accountObject.getBalance());
+        userRepository.save(user);
       return accountRepository.save(accountObject);
     }
   }
-
 
     /**
      * <h1> Update Account </h1>
@@ -101,6 +114,13 @@ public class AccountService {
     if (account == null) {
       throw new InformationNotFoundException("account with id " + accountId + " not found");
     } else {
+        // update the user Total Balance
+        User user = userDetails.getUser();
+        // Take the current account balance from user Total Balance
+        user.setTotalBalance(user.getTotalBalance() - account.getBalance());
+        // Add the new account balance to user Total Balance
+        user.setTotalBalance(user.getTotalBalance() + accountObject.getBalance());
+        userRepository.save(user);
       account.setName(accountObject.getName());
       account.setBalance(accountObject.getBalance());
       account.setUser(userDetails.getUser());
@@ -116,7 +136,12 @@ public class AccountService {
     if (account == null) {
       throw new InformationNotFoundException("account with id " + accountId + " Not Found... :(");
     } else {
-//      **** DeleteById() ****
+        // update the user Total Balance
+        User user = userDetails.getUser();
+        // Take the current account balance from user Total Balance
+        user.setTotalBalance(user.getTotalBalance() - account.getBalance());
+        userRepository.save(user);
+      // **** DeleteById() ****
       accountRepository.deleteById(accountId);
       return "Account ID: " + accountId + " has been successfully deleted.";
     }
@@ -146,23 +171,64 @@ public class AccountService {
     //    get account info;
     //    account.balance()
 //    System.out.println(account.getBalance());
-    if (transactionObject.getType().toLowerCase().equals("withdraw")) { // check transaction type
-        // check if balance less than withdraw
-        // Get withdraw
-        double withdraw = transactionObject.getAmount();
-        double totalBalance = accountRepository.findAllBalanceById(accountId);
+      // update the user Total Balance
+      User user = userDetails.getUser();
 
-      if(withdraw < account.getBalance()){
+      // Take the current account balance from user Total Balance
+      double totalBalance = user.getTotalBalance();
+      // Get withdraw or deposit amount
+      double transactionAmount = transactionObject.getAmount();
+      /*
+       *       withdraw
+       */
+    if (transactionObject.getType().toLowerCase().equals("withdraw")) {
+
+      // check if balance less than withdraw
+      if(transactionAmount < account.getBalance()){
           account.setBalance(account.getBalance() - transactionObject.getAmount());
           accountRepository.save(account);
+          // update the user Total Balance
+          // Take the current account balance from user Total Balance
+          user.setTotalBalance(user.getTotalBalance() - account.getBalance());
+          userRepository.save(user);
       }// check if balance less than all total balance of all accounts
-      else if(withdraw < totalBalance){
+      else if(transactionAmount < totalBalance){
 
-          double amountLeftToWithdraw = withdraw - account.getBalance();
+          double amountLeftToWithdraw = transactionAmount - account.getBalance();
+          // update the user Total Balance
+          // Take the current account balance from user Total Balance
+          user.setTotalBalance(user.getTotalBalance() - account.getBalance());
+          userRepository.save(user);
+
           account.setBalance(0.0);
           accountRepository.save(account);
           // check for any other balance
           // loop in to the other account until all amountLeftToWithdraw reach
+          List <Account> accounts = user.getAccountList();
+          int i =0 ;
+          while (amountLeftToWithdraw > 0 && i < accounts.size() ){
+              if(accounts.get(i).getBalance() > amountLeftToWithdraw){
+                  // update the user Total Balance
+                  // Take the current account balance from user Total Balance
+                  user.setTotalBalance(user.getTotalBalance() - amountLeftToWithdraw);
+                  userRepository.save(user);
+                  accounts.get(i).setBalance(accounts.get(i).getBalance() - amountLeftToWithdraw);
+                  // save
+                  accountRepository.save(accounts.get(i));
+
+                  amountLeftToWithdraw = 0;
+
+              }else {
+                  // Take the current account balance from user Total Balance
+                  user.setTotalBalance(user.getTotalBalance() - amountLeftToWithdraw);
+                  amountLeftToWithdraw = amountLeftToWithdraw - accounts.get(i).getBalance();
+                  userRepository.save(user);
+                  accounts.get(i).setBalance(0.0);
+                  accountRepository.save(accounts.get(i));
+              }
+              i++;
+          }
+
       }
       else{
           throw new InsufficientResources("Balance Insufficient");
@@ -170,7 +236,10 @@ public class AccountService {
         //save new balance to account
     }
       else if (transactionObject.getType().toLowerCase().equals("deposit")) { // check transaction type
+          user.setTotalBalance(user.getTotalBalance() + transactionAmount);
+          userRepository.save(user);
           account.setBalance(account.getBalance() + transactionObject.getAmount()); // addition transaction amt from account balance
+          accountRepository.save(account);
       }
 
     transactionObject.setUser(userDetails.getUser());
@@ -236,4 +305,12 @@ public class AccountService {
       return transactionRepository.save(transaction);
   }
 
+//  User updateTotalBalance(){
+//      MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//      LOGGER.info("Updating Transaction from Service...");
+//      User user = userDetails.getUser();
+//      List <Account> accounts = user.getAccountList();
+//      for(int i = 0;)
+//      user.setTotalBalance(user.getTotalBalance() - account.getBalance());
+//  }
 }
